3d871e3337794986887b033bce34605daa401908
SoufianeAbo <abounasrsoufiane@gmail.com>
2024-04-19 14:56:01 +0100

    finish careers page

--- resources/views/careers.blade.php
+++ resources/views/careers.blade.php
@@ -97,7 +97,7 @@
         </div>
 
         <div class = "mt-24 flex flex-row-reverse px-24 gap-8">
-            <img class = "w-1/2" src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/25/36/5f/united-nations-headquarters.jpg?w=1200&h=-1&s=1" alt="">
+            <img class = "size-1/2" src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/25/36/5f/united-nations-headquarters.jpg?w=1200&h=-1&s=1" alt="">
             <div class = "flex flex-col items-start">
                 <img class = "object-none" src="branddots.png" alt="">
                 <p class = "boldfour text-[2.5rem] mb-4">Located In Irvine, California</p>
@@ -114,5 +114,96 @@
                     halls.</p>
             </div>
         </div>
+    </div>
+    <div class = "flex flex-col items-center mt-14">
+        <img src="branddots.png" alt="">
+        <p class = "boldfour text-[2.5rem] mt">Job Openings</p>
+        <p class = "text-md">Find your next role with Neoline Studios</p>
+        <div class = "w-3/4 pt-4 pb-2 flex flex-col border-b-2 border-neutral-300">
+            <div class = "flex flex-row justify-between items-center">
+                <p class = "text-xl boldfour">Artist</p>
+                <button class = "text-white bg-black rounded-full px-6 py-2 boldmedium transition-all hover:text-black border-black hover:bg-white border-2">Apply</button>
+            </div>
+        </div>
+
+        <div class = "w-3/4 pt-4 pb-2 flex flex-col border-b-2 border-neutral-300">
+            <div class = "flex flex-row justify-between items-center">
+                <p class = "text-xl boldfour">Story Designer</p>
+                <button class = "text-white bg-black rounded-full px-6 py-2 boldmedium transition-all hover:text-black border-black hover:bg-white border-2">Apply</button>
+            </div>
+        </div>
+
+        <div class = "w-3/4 pt-4 pb-2 flex flex-col border-b-2 border-neutral-300">
+            <div class = "flex flex-row justify-between items-center">
+                <p class = "text-xl boldfour">Proofreader</p>
+                <button class = "text-white bg-black rounded-full px-6 py-2 boldmedium transition-all hover:text-black border-black hover:bg-white border-2">Apply</button>
+            </div>
+        </div>
+
+        <div class = "w-3/4 pt-4 pb-2 flex flex-col border-b-2 border-neutral-300">
+            <div class = "flex flex-row justify-between items-center">
+                <p class = "text-xl boldfour">Voice Actor</p>
+                <button class = "text-white bg-black rounded-full px-6 py-2 boldmedium transition-all hover:text-black border-black hover:bg-white border-2">Apply</button>
+            </div>
+        </div>
+
+        <div class = "w-3/4 pt-4 pb-2 flex flex-col border-b-2 border-neutral-300">
+            <div class = "flex flex-row justify-between items-center">
+                <p class = "text-xl boldfour">Game Designer</p>
+                <button class = "text-white bg-black rounded-full px-6 py-2 boldmedium transition-all hover:text-black border-black hover:bg-white border-2">Apply</button>
+            </div>
+        </div>
+
+        <div class = "w-3/4 pt-4 pb-2 flex flex-col border-b-2 border-neutral-300">
+            <div class = "flex flex-row justify-between items-center">
+                <p class = "text-xl boldfour">Project Designer</p>
+                <button class = "text-white bg-black rounded-full px-6 py-2 boldmedium transition-all hover:text-black border-black hover:bg-white border-2">Apply</button>
+            </div>
+        </div>
+
+        <div class = "w-3/4 pt-4 pb-2 flex flex-col border-b-2 border-neutral-300">
+            <div class = "flex flex-row justify-between items-center">
+                <p class = "text-xl boldfour">Producer</p>
+                <button class = "text-white bg-black rounded-full px-6 py-2 boldmedium transition-all hover:text-black border-black hover:bg-white border-2">Apply</button>
+            </div>
+        </div>
+    </div>
+
+    <div class = "flex flex-col mt-14 items-center px-32">
+        <img src="branddots.png" alt="">
+        <p class = "boldfour text-[2.5rem]">Equal Opportunity Employer</p>
+        <p class = "text-md text-center">We are an equal opportunity employer and make employment decisions on the basis of merit. 
+            We do not discriminate based on race, ancestry, sex, gender, gender identification or expression, 
+            genetic characteristics, religion (including religious dress and grooming practices), age, military
+            or veteran status, color, marital status, sexual orientation, pregnancy, disability, registered domestic
+            partner status, national origin, or any other category protected by law. </p>
+    </div>
+    <div class="bg-black p-8 mt-16">
+        <div class="flex flex-row text-white logo items-start justify-between">
+            <div class="flex flex-row items-center gap-4">
+                <img class="w-16" src="neolinewhiteonly.png" alt="">
+                <p class="text-xl">Neoline</p>
+            </div>
+
+            <div class = "flex flex-col gap-4">
+                <a href="#">Games</a>
+                <a href="{{ route('careers.index') }}">Careers</a>
+                <a href="#">News</a>
+                <a href="#">Login</a>
+            </div>
+
+            <div class="flex flex-col footer w-1/2">
+                <p class="text-sm tracking-wider w-fit">Our partners</p>
+                <div class="flex flex-row w-full gap-4 items-center">
+                    <img class="w-1/6 h-full" src="steamlogo.png" alt="">
+                    <img class="w-1/12" src="https://fineproxy.org/wp-content/uploads/2023/08/Epic-Games-logo.png" alt="">
+                    <img class="w-1/6" src="https://www.storylandstudios.com/wp-content/uploads/2022/01/Ubisoft-Horizontal-Logo-WHITE_1.png" alt="">
+                    <img class="w-1/12" src="https://vgboxart.com/resources/logo/12498_ea-white-prev.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bg-[#e20613] footer">
+        <p class="text-white p-4 text-sm tracking-wider">© 2010 - 2024 Neoline LLC</p>
     </div>
 </body>
