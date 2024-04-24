39b2097899d71893742d9091f92ae6e3bc0d4354
SoufianeAbo <abounasrsoufiane@gmail.com>
2024-04-24 16:56:34 +0100

    add models for platform and gameplatforms

---
+++ app/Models/GamePlatforms.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class GamePlatforms extends Model
+{
+    use HasFactory;
+
+    protected $table = 'gameplatforms';
+
+    protected $fillable = [
+        'gameID',
+        'platformID',
+    ];
+
+    public function game(): BelongsTo
+    {
+        return $this->belongsTo(Game::class, 'gameID');
+    }
+
+    public function platformID(): BelongsTo
+    {
+        return $this->belongsTo(Platforms::class, 'platformID');
+    }
+}
