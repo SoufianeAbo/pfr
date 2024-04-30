1befa455cc5d5d3aa969b2d7ba4c05c23562c8a8
SoufianeAbo <abounasrsoufiane@gmail.com>
2024-04-30 11:34:57 +0100

    add sesssion

--- resources/views/employee/dashboard.blade.php
+++ resources/views/employee/dashboard.blade.php
@@ -11,9 +11,9 @@
                 <div
                     class="flex flex-row-reverse gap-4 items-center justify-center bg-gradient-to-r from-[#f90617] to-[#f91f2e] mx-4 py-2 rounded-lg">
                     <img class="w-1/4 rounded-full ring ring-white"
-                        src="{{ asset('storage/' . $employee->application->picture) }}"
-                        alt="">
-                    <p class="text-white">{{ $employee->application->firstName }} {{ $employee->application->lastName }}</p>
+                        src="{{ asset('storage/' . $employee->application->picture) }}" alt="">
+                    <p class="text-white">{{ $employee->application->firstName }} {{ $employee->application->lastName }}
+                    </p>
                 </div>
             </div>
 
@@ -33,6 +33,15 @@
         </div>
     </div>
     <div class="pt-8 w-11/12">
+        @if (session('success'))
+            <div class="ml-72 flex items-center p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50"
+                role="alert">
+                <i class = "fas fa-check mr-2"></i>
+                <div>
+                    <span class="font-medium">Success!</span> {{ session('success') }}
+                </div>
+            </div>
+        @endif
         <div class="ml-72 bg-neutral-100 rounded-lg h-screen">
             <h1 class="text-3xl font-bold pt-8 pl-4">Applications</h1>
             <div class="grid grid-cols-3 gap-4 mx-4 pt-8">
@@ -47,69 +56,92 @@
                                 </div>
                             </div>
                         </div>
-                        <p class = "text-sm"><b>Applied Role:</b> {{ $applicant->role->roleName }}</p>
-                        <p class = "text-sm"><b>Source:</b> {{ $applicant->source }}</p>
-                        <p class = "text-sm"><b>Date Applied:</b> {{ $applicant->created_at }}</p>
+                        <p class="text-sm"><b>Applied Role:</b> {{ $applicant->role->roleName }}</p>
+                        <p class="text-sm"><b>Source:</b> {{ $applicant->source }}</p>
+                        <p class="text-sm"><b>Date Applied:</b> {{ $applicant->created_at }}</p>
                         <div class="daisy-card-actions justify-end">
-                            <label for = "modal_{{ $applicant->id }}" class="bg-[#f90617] cursor-pointer px-4 py-2 rounded-lg text-white">Details</label>
+                            <label for="modal_{{ $applicant->id }}"
+                                class="bg-[#f90617] cursor-pointer px-4 py-2 rounded-lg text-white">Details</label>
                             <input type="checkbox" id="modal_{{ $applicant->id }}" class="daisy-modal-toggle" />
                             <dialog class="daisy-modal bg-black bg-opacity-20">
-                            <div class="daisy-modal-box">
-                                <div class = "flex flex-row items-center justify-between">
-                                    <h3 class="font-bold text-5xl">{{ $applicant->firstName }} {{ $applicant->lastName }}</h3>
-                                    <div class="daisy-avatar">
-                                        <div class="w-24 rounded-full">
-                                            <img src="{{ asset('storage/' . $applicant->picture) }}" />
+                                <div class="daisy-modal-box">
+                                    <div class="flex flex-row items-center justify-between">
+                                        <h3 class="font-bold text-5xl">{{ $applicant->firstName }} {{
+                                            $applicant->lastName }}</h3>
+                                        <div class="daisy-avatar">
+                                            <div class="w-24 rounded-full">
+                                                <img src="{{ asset('storage/' . $applicant->picture) }}" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <p class="pt-4 pb"><b>First Name:</b> {{ $applicant->firstName }}</p>
+                                    <p class="py"><b>Last Name:</b> {{ $applicant->lastName }}</p>
+                                    <p class="py"><b>Email:</b> {{ $applicant->email }}</p>
+                                    <p class="py"><b>Phone:</b> {{ $applicant->phone }}</p>
+                                    <p class="py"><b>City:</b> {{ $applicant->city }}</p>
+
+                                    @if ($applicant->coverLetter == null)
+                                    <p class="py"><b>Cover Letter:</b><em class="text-xs text-gray-500"> none
+                                            attached</em></p>
+                                    @else
+                                    <p class="py"><b>Cover Letter:</b><a
+                                            href="{{ asset('storage/' . $applicant->coverLetter) }}" download><em
+                                                class="text-xs text-blue-500 underline"> Download here</em></a></p>
+                                    @endif
+
+                                    <p class="py"><b>Resume/CV:</b><a
+                                            href="{{ asset('storage/' . $applicant->resume) }}" download><em
+                                                class="text-xs text-blue-500 underline"> Download here</em></a></p>
+                                    <p class="py"><b>Country:</b> {{ $applicant->country }}</p>
+
+                                    @if ($applicant->linkedinProfile == null)
+                                    <p class="py"><b>LinkedIn Profile:</b><em class="text-xs text-gray-500"> none
+                                            attached</em></p>
+                                    @else
+                                    <p class="py"><b>LinkedIn Profile:</b><a
+                                            href="{{ $applicant->linkedinProfile }}"><em
+                                                class="text-xs text-blue-500"><i class="fab fa-linkedin ml-2"></i> Visit
+                                                link!</em></a></p>
+                                    @endif
+
+                                    @if ($applicant->portfolio == null)
+                                    <p class="py"><b>Portfolio:</b><em class="text-xs text-gray-500"> none attached</em>
+                                    </p>
+                                    @else
+                                    <p class="py"><b>Portfolio:</b><a href="{{ $applicant->linkedinProfile }}"><em
+                                                class="text-xs text-blue-500"><i class="fas fa-paperclip ml-2"></i>
+                                                Visit link!</em></a></p>
+                                    @endif
+                                    <p class="py"><b>Salary Expectation:</b>
+                                    <p class="text-green-500">{{ number_format($applicant->salaryExpectation) }}$</p>
+                                    </p>
+                                    <p class="py-4"><b>Position:</b> {{ $applicant->role->roleName }}</p>
+                                    <div class="flex flex-row-reverse justify-between">
+                                        <div class="daisy-modal-action">
+                                            <label class="bg-[#f90617] cursor-pointer px-4 py-2 rounded-lg text-white"
+                                                for="modal_{{ $applicant->id }}">Close</label>
+                                        </div>
+
+                                        <div class="flex flex-row daisy-modal-action">
+                                            <form method="POST" action="{{ route('hr.accept') }}">
+                                                @csrf
+                                                <input class="hidden" name="applicationID" type="text"
+                                                    value="{{ $applicant->id }}">
+                                                <button type="submit"
+                                                    class="bg-green-600 cursor-pointer px-4 py-2 rounded-lg text-white"><i
+                                                        class="fas fa-check mr-2"></i>Accept</button>
+                                            </form>
+                                            <form method="POST" action="{{ route('hr.reject') }}">
+                                                @csrf
+                                                <input class="hidden" name="applicationID" type="text"
+                                                    value="{{ $applicant->id }}">
+                                                <button type="submit"
+                                                    class="bg-[#f90617] cursor-pointer px-4 py-2 rounded-lg text-white"><i
+                                                        class="fas fa-xmark mr-2"></i>Reject</button>
+                                            </form>
                                         </div>
                                     </div>
                                 </div>
-                                <p class="pt-4 pb"><b>First Name:</b> {{ $applicant->firstName }}</p>
-                                <p class="py"><b>Last Name:</b> {{ $applicant->lastName }}</p>
-                                <p class="py"><b>Email:</b> {{ $applicant->email }}</p>
-                                <p class="py"><b>Phone:</b> {{ $applicant->phone }}</p>
-                                <p class="py"><b>City:</b> {{ $applicant->city }}</p>
-                                
-                                @if ($applicant->coverLetter == null)
-                                    <p class="py"><b>Cover Letter:</b><em class = "text-xs text-gray-500"> none attached</em></p>
-                                @else
-                                    <p class="py"><b>Cover Letter:</b><a href="{{ asset('storage/' . $applicant->coverLetter) }}" download><em class = "text-xs text-blue-500 underline"> Download here</em></a></p>
-                                @endif
-
-                                <p class="py"><b>Resume/CV:</b><a href="{{ asset('storage/' . $applicant->resume) }}" download><em class = "text-xs text-blue-500 underline"> Download here</em></a></p>
-                                <p class="py"><b>Country:</b> {{ $applicant->country }}</p>
-
-                                @if ($applicant->linkedinProfile == null)
-                                    <p class="py"><b>LinkedIn Profile:</b><em class = "text-xs text-gray-500"> none attached</em></p>
-                                @else
-                                    <p class="py"><b>LinkedIn Profile:</b><a href="{{ $applicant->linkedinProfile }}"><em class = "text-xs text-blue-500"><i class = "fab fa-linkedin ml-2"></i> Visit link!</em></a></p>
-                                @endif
-
-                                @if ($applicant->portfolio == null)
-                                    <p class="py"><b>Portfolio:</b><em class = "text-xs text-gray-500"> none attached</em></p>
-                                @else
-                                    <p class="py"><b>Portfolio:</b><a href="{{ $applicant->linkedinProfile }}"><em class = "text-xs text-blue-500"><i class = "fas fa-paperclip ml-2"></i> Visit link!</em></a></p>
-                                @endif
-                                <p class="py"><b>Salary Expectation:</b> <p class = "text-green-500">{{ number_format($applicant->salaryExpectation) }}$</p></p>
-                                <p class="py-4"><b>Position:</b> {{ $applicant->role->roleName }}</p>
-                                <div class = "flex flex-row-reverse justify-between">
-                                    <div class="daisy-modal-action">
-                                        <label class="bg-[#f90617] cursor-pointer px-4 py-2 rounded-lg text-white" for = "modal_{{ $applicant->id }}">Close</label>
-                                    </div>
-
-                                    <div class = "flex flex-row daisy-modal-action">
-                                        <form method = "POST" action="{{ route('hr.accept') }}">
-                                            @csrf
-                                            <input class = "hidden" name = "applicationID" type="text" value = "{{ $applicant->id }}" >
-                                            <button type = "submit" class = "bg-green-600 cursor-pointer px-4 py-2 rounded-lg text-white"><i class = "fas fa-check mr-2"></i>Accept</button>
-                                        </form>
-                                        <form method = "POST" action="{{ route('hr.reject') }}">
-                                            @csrf
-                                            <input class = "hidden" name = "applicationID" type="text" value = "{{ $applicant->id }}">
-                                            <button type = "submit" class = "bg-[#f90617] cursor-pointer px-4 py-2 rounded-lg text-white"><i class = "fas fa-xmark mr-2"></i>Reject</button>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
                             </dialog>
                         </div>
                     </div>
